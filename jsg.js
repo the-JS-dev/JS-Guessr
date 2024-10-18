e97d8dd97c9d5131cbd7b90701a375d8d4e4bcb3
edH <17eyatah@starmirdif.com>
2024-10-18 09:18:51 +0400

    Add files via upload

---
+++ jsg.js
@@ -0,0 +1,34 @@
+var answer = parseInt(Math.random() * 100);
+var numAttempts = 0;
+console.log(answer);
+var answerISCorrect = false;
+function guess() {
+    var rawField = document.getElementById("guess");
+    var naturalField = parseInt(rawField.value);
+    if (answerISCorrect === false) {
+        if (naturalField >= 100) {
+            window.alert("The number can only be between 1 and 100!");
+        } else {
+            if (naturalField === answer) {
+                numAttempts += 1;
+                document.getElementById("aC").innerHTML = numAttempts;
+                window.alert("Congratulations! You guessed the number! Reload the page to play again...");
+                answerISCorrect = true;
+            } else {
+                if (naturalField >= answer) {
+                    numAttempts += 1;
+                    document.getElementById("aC").innerHTML = numAttempts;
+                    window.alert("The number is too big... try again!");
+                } else {
+                    if (naturalField <= answer) {
+                        numAttempts += 1;
+                        document.getElementById("aC").innerHTML = numAttempts;
+                        window.alert("The number is too small... try again!");
+                    } else {
+                        window.alert("That isn't a number... try again!");
+                    }
+                }
+            }
+        }
+    }
+}
